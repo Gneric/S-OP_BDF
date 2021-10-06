cb55e86061ad1e62324c2175c847058f404f5b90
Gneric <obiondii99@gmail.com>
2021-10-06 09:31:54 -0500

    serve

--- main.py
+++ main.py
@@ -29,6 +29,6 @@ api.add_resource(LogIn, '/api/auth/login')
 api.add_resource(SignIn, '/api/auth/signin')
 
 if __name__ == '__main__':
-  #from waitress import serve
-  #serve(app, host="0.0.0.0", port=3100, threads=8)
-  app.run(host='0.0.0.0', port=3100, debug=True)
+  from waitress import serve
+  serve(app, host="0.0.0.0", port=3100, threads=8)
+  #app.run(host='0.0.0.0', port=3100, debug=True)
