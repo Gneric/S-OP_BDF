232fc5cc889f81c9f2e9faa76b4117c3b91d5aa3
Gneric <obiondii99@gmail.com>
2021-10-20 13:57:19 -0500

    cambio password

--- api/routes/changepwd.py
+++ api/routes/changepwd.py
@@ -13,7 +13,13 @@ class ChangePassword(Resource):
             user_id = request.json.get('user_id','')
             pwd = request.json.get('password','')
             new_pwd = request.json.get('new_password','')
-            if current_user == user_id:
+            if current_user == 1:
+                confirm_pwd = ""
+            else:
+                confirm_pwd = request.json.get('confirm_password', '')
+                if confirm_pwd != new_pwd:
+                    return { 'error': 'los campos de nueva contraseña no coinciden'}, 400
+            if current_user == user_id or current_user == 1:
                 return pwdChange(user_id, pwd, new_pwd)
             else:
                 return {"msg": "No tiene permisos para hacer cambios en este usuario"}, 401
