5f8c0c96095af2da2c008eab5e0b693097b88cf7
Gneric <obiondii99@gmail.com>
2021-11-18 11:35:09 -0500

    (FCSimulation) Añadiendo FCSimulation

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from datetime import timedelta
 from api.routes.login import LogIn
 from api.routes.welcome import Welcome
 from api.routes.users import ChangePassword, ModifyUser, CreateUser, UserList
-from api.routes.visuals import DemandSimulation, GetBDHistorico, GetVisualBD, PrepareSummary, NetSalesxPBU, UnitsxBPU
+from api.routes.visuals import DemandSimulation, FCSimulation, GetBDHistorico, GetVisualBD, PrepareSummary, NetSalesxPBU, UnitsxBPU
 from api.routes.permissions import GetPermissions, UpdatePermissions
 from api.routes.data import AddRow, GetData, DeleteData, CloneData, UpdateDbData, UploadExcel, GetTemplates, GetInfoMes
 
@@ -89,6 +89,7 @@ api.add_resource(PrepareSummary, '/api/prepare_summary')
 api.add_resource(UnitsxBPU, '/api/unitxbpu')
 api.add_resource(NetSalesxPBU, '/api/salesxbpu')
 api.add_resource(DemandSimulation, '/api/demand_simulation')
+api.add_resource(FCSimulation, '/api/fc_simulation')
 
 
 
