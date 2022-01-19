17cf3254dcb7865e2d440d9f949b07e34d1ea0b6
Gneric <obiondii99@gmail.com>
2022-01-19 10:14:33 -0500

    GetSet timeline cambio

--- api/utils/hasura_api.py
+++ api/utils/hasura_api.py
@@ -1253,8 +1253,9 @@ def requestinfo_timeline(permissionID, timelineID):
         }
         """
         res = queryHasura(query, {'permissionID': permissionID, 'timelineID': timelineID})
-        permissions = res['data']['permissions'][0]['isBlocked']
-        timeline = res['data']['timeline'][0]['estado']
+        print(res)
+        permissions = res['data']['Permissions'][0]['isBlocked']
+        timeline = res['data']['Timeline'][0]['estado']
         return { 'isBlocked': permissions, 'estado': timeline }
     except:
         return {}
