5da3a0ba3b24e984df41dc64ec0b069e9031e667
Gneric <obiondii99@gmail.com>
2022-02-17 13:10:17 -0500

    requestPrepareSummary añadido canal

--- api/utils/hasura_api.py
+++ api/utils/hasura_api.py
@@ -781,6 +781,7 @@ def requestPrepareSummary(id):
         rows: BD_Prepare_Summary(where: {id:{_eq:$id}}) {
             input
             nart
+            canal
             descripcion
             year
             mes
